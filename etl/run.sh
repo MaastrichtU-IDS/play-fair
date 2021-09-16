a282918c48c8b736400c9e890e0baea4e800e96b
Vincent Emonet <vincent.emonet@gmail.com>
2021-09-16 14:57:20 +0000

    improve dsri run

--- etl/run.sh
+++ etl/run.sh
@@ -6,6 +6,7 @@ then
     echo "Provide the path to a YARRRML file"
     exit
 fi
+mkdir -p data
 mkdir -p output
 
 rml_mappings=data/$(echo $yarrrml_mappings | sed "s/\.yarrr\.yml/.rml.ttl/g" )
@@ -13,4 +14,4 @@ rdf_output=output/$(echo $yarrrml_mappings | sed "s/\.yarrr\.yml/-output.ttl/g" 
 
 yarrrml-parser -i $yarrrml_mappings -o $rml_mappings
 
-java -jar /opt/rmlmapper.java -i $rml_mappings -o $rdf_output
+java -jar /opt/rmlmapper.jar -m $rml_mappings -o $rdf_output
